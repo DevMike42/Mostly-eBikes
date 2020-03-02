75a28ac6b9f1ae711d8cd739dcf54c2bf4c8e505
Mike Rivera <michaelstephenrivera@gmail.com>
2020-03-01 17:21:50 -0800

    Added links to Google Analytics

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,3 +6,7 @@ $('.carousel').carousel({
 
 // Get the current year for the copyright
 $('#year').text(new Date().getFullYear());
+
+window.dataLayer = window.dataLayer || [];
+function gtag() { dataLayer.push(arguments); }
+gtag('js', new Date());
